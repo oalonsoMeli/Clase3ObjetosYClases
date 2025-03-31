39185d108ef60b31f0100266c346073deba1b891
oalonsoMeli <ornella.alonso@mercadolibre.com>
2025-03-31 12:07:06 -0300

    Update Main.java

--- src/bootcamp/Main.java
+++ src/bootcamp/Main.java
@@ -6,5 +6,6 @@ public class Main {
         Persona persona2 = new Persona("Ornella", 22, "44847330");
         Persona persona3 = new Persona("Raul", "283749583", 45, 70.0, 1.70);
         System.out.println(persona3);
+
     }
 }
